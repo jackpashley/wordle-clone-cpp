fe24019bb9a9532f46b56571be09dcae16ac2ef7
jackpashley <jack.t.pashley@gmail.com>
2022-01-21 00:46:02 +1000

    virtually finished

--- main.cpp
+++ main.cpp
@@ -11,16 +11,31 @@ std::string determineWord(void) {
 }
 
 void evalAccuracy(std::string guessedWord, std::string word) {
-	for (char& c : guessedWord) {
-
+	for (int i = 0; i < guessedWord.length() - 1; i++) {
+		char letter = guessedWord[i];
+			// Check if they got any RIGHT
+			if (guessedWord[i] == word[i]) {
+				std::cout << "You got: " << guessedWord[i]
+					<< " in the correct position." << std::endl;
+			}
+			else if (word.find(letter) != std::string::npos) {
+				std::cout << letter << " is in the word but you got it in the wrong position"
+					<< std::endl;
+			}
 	}
 }
 
 int main(void) {
+	time_t t;
+	srand((unsigned)time(&t));
 	// First determine which is the word of the day
 	std::string word = determineWord();
 	std::string guess;
 	std::cout << "The word is " << word.length() << " characters long. ";
+
+	// Debug
+	std::cout << "The random word is " << word << std::endl;
+	// END debug
 
 	while (guess != word) {
 		std::cout << "Enter your guess: ";
@@ -28,6 +43,10 @@ int main(void) {
 		std::cout << "\n You guessed " << guess;
 		std::cout << std::endl;
 		// Evaluate accuracy
+		if (guess.length() == word.length())
+			evalAccuracy(guess, word);
+		else if (guess.length() != word.length())
+			printf("You must enter a word that is %d letters long \n", word.length());
 	}
 	
 
